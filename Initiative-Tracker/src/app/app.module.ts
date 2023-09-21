6421ac4d29822d6566bd9591f74a4779dd3f81cf
Ryan Pointer <ryankeithpointer@gmail.com>
2023-09-21 11:50:18 -0500

    Added Angular Material

--- Initiative-Tracker/src/app/app.module.ts
+++ Initiative-Tracker/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TitlePageComponent } from './tools/title-page/title-page.component';
 import { UnitComponent } from './tools/unit/unit.component';
 import { AddUnitComponent } from './tools/add-unit/add-unit.component';
 import { SaveUnitComponent } from './tools/save-unit/save-unit.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { SaveUnitComponent } from './tools/save-unit/save-unit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
